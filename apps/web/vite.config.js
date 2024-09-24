c4ef23872634d4bb05abcc2c75b7822889484da6
Nic Strong <nic.strong@ipfx.com>
2024-09-24 21:40:50 +1200

    CHanges to vite build config

---
+++ apps/web/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = dirname(__filename);
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: "./runtimeConfig",
+                replacement: "./runtimeConfig.browser",
+            },
+            // Reference: https://github.com/vercel/turbo/discussions/620#discussioncomment-2136195
+            {
+                find: "@ui",
+                replacement: path.resolve(__dirname, "../../packages/ui/src"),
+            },
+        ],
+    },
+});
