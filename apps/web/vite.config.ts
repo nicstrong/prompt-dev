11cf335ffc6921f3a02e18a511d94e3ee70a4cc7
Nic Strong <nic.strong@ipfx.com>
2025-03-07 23:27:57 +1300

    Add tailwind 4 and shadcn

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,35 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path, { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser',
-      },
-      // Reference: https://github.com/vercel/turbo/discussions/620#discussioncomment-2136195
-      {
-        find: '@ui',
-        replacement: path.resolve(__dirname, '../../packages/ui/src'),
-      },
-    ],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler', // or "modern", "legacy"
-        importers: [
-          // ...
-        ],
-      },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
