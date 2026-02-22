2b6f1eadec3fefcfe0d9796d57d98cbc47e9c257
Nic Strong <nic.strong@ipfx.com>
2026-02-22 17:07:24 +1300

    AI SDK v6 update

--- apps/web/src/components/Chat/Chat.tsx
+++ apps/web/src/components/Chat/Chat.tsx
@@ -9,52 +9,70 @@ import {
 } from '@/components/ai-elements/conversation'
 
 import { Loader } from '@/components/ai-elements/loader'
+import { cn } from '@/lib/utils'
 
 export function Chat() {
   const { messages, status } = useChatContext()
-  const { stableMessages, generatingMessage } = useStableMessages(
-    status,
-    messages,
-  )
-  useEffect(() => {
-    if (generatingMessage) {
-      const summary = generatingMessage.parts
-        .map((message) => {
-          if (message.type === 'text') {
-            return `${message.type}: ${message.text.length > 20 ? `...${message.text.substring(message.text.length - 20)}` : message.text}`
-          }
-          return `${message.type}:`
-        })
-        .join('\n')
-      console.log(`[Chat] generatingMessage:\n${summary}`)
-    }
-  }, [generatingMessage])
+  // const { stableMessages, generatingMessage } = useStableMessages(
+  //   status,
+  //   messages,
+  // )
 
-  const isGenerating =
-    status === 'submitted' ||
-    (status === 'streaming' &&
-      generatingMessage !== undefined &&
-      generatingMessage.parts.every((part) => part.type !== 'text'))
+  // const isGeneratingFirstTextPart =
+  //   status === 'submitted' ||
+  //   (status === 'streaming' &&
+  //     generatingMessage !== undefined &&
+  //     generatingMessage.parts.every((part) => part.type !== 'text'))
+
+  // useEffect(() => {
+  //   if (generatingMessage) {
+  //     const summary = generatingMessage.parts
+  //       .filter((p) => p.type === 'text')
+  //       .map((message) => {
+  //         return `${message.type}: ${message.text.length > 20 ? `...${JSON.stringify(message.text.substring(message.text.length - 20))}` : JSON.stringify(message.text)}`
+  //       })
+  //       .join('\n')
+  //     console.log(
+  //       `[Chat] generatingMessage(${isGeneratingFirstTextPart}): ${summary}`,
+  //     )
+  //   }
+  // }, [generatingMessage, isGeneratingFirstTextPart])
 
   return (
-    <Conversation className='relative overflow-y-auto pb-32'>
-      <ConversationContent className='pb-10-pt-safe-offset-10 mx-auto flex w-full max-w-4xl flex-col space-y-4 px-4'>
-        {stableMessages.map((message) =>
+    // <Conversation
+    //   className={cn(
+    //     'relative overflow-y-auto pb-32',
+    //     !isGeneratingFirstTextPart && generatingMessage && 'bg-red-800',
+    //   )}
+    // >
+    <div className='relative overflow-y-auto pb-32'>
+      {/* <ConversationContent className='pb-10-pt-safe-offset-10 mx-auto flex w-full max-w-4xl flex-col space-y-4 px-4'> */}
+      <div className='pb-10-pt-safe-offset-10 mx-auto flex w-full max-w-4xl flex-col space-y-4 px-4'>
+        {messages.map((message, index) =>
           message.role === 'user' ? (
             <UserMessage key={message.id} message={message} />
           ) : (
-            <AssistantMessage key={message.id} message={message} />
+            <AssistantMessage
+              key={message.id}
+              message={message}
+              debug={index === messages.length - 1}
+            />
           ),
         )}
-        {!isGenerating && generatingMessage && (
+        {/* {!isGeneratingFirstTextPart && generatingMessage && (
           <AssistantMessage
             key={generatingMessage.id}
             message={generatingMessage}
+            debug
           />
-        )}
-        {isGenerating && <Loader size={32} className='fill-current' />}
-      </ConversationContent>
-      <ConversationScrollButton className='hover:bg-secondary bottom-36 backdrop-blur-xl' />
-    </Conversation>
+        )} */}
+        {/* {isGeneratingFirstTextPart && (
+          <Loader size={32} className='fill-current' />
+        )} */}
+        {/* </ConversationContent> */}
+      </div>
+      {/* <ConversationScrollButton className='hover:bg-secondary bottom-36 backdrop-blur-xl' /> */}
+      {/* </Conversation> */}
+    </div>
   )
 }
